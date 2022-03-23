d1f6a202758494ba5167591a33be59f6e888ab89
ArifAhmed <arif15-12301@diu.edu.bd>
2022-03-23 14:07:10 +0600

    2 PROBLEM SOLVED AFTER A LONG TIME
    Ez Question

---
+++ ArifAhmed/src/b_1589.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class b_1589 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int T= in.nextInt();
+		for(int i = 0; i< T; i++) {
+			int R1 = in.nextInt();
+			int R2 = in.nextInt();
+			
+			System.out.println(R1+R2);
+		}
+
+	}
+
+}
---
+++ ArifAhmed/src/beecrowd_1564.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class beecrowd_1564 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		while(in.hasNext()) {
+			int N = in.nextInt();
+			if(N==0) System.out.println("vai ter copa!");
+			else System.out.println("vai ter duas!");
+		}
+		
+
+	}
+
+}
