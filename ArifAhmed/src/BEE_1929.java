d7697d46cd2cdbba8e4ee6c02e4139f3e0782145
ArifAhmed <arif15-12301@diu.edu.bd>
2022-05-19 18:42:25 +0600

    solved

--- ArifAhmed/src/BEE_1929.java
+++ ArifAhmed/src/BEE_1929.java
@@ -4,6 +4,58 @@ public class BEE_1929 {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		int A,B,C,D;
+		A = in.nextInt();
+		B = in.nextInt();
+		C = in.nextInt();
+		D = in.nextInt();
+		int i = 0;
+		
+		
+			if(A+B>C) {
+				if(B+C>A) {
+					if(C+A>B) {
+						System.out.println("S");
+						i++;
+						
+						
+					}
+				}
+			}
+			if(D+B>C & i<=0) {
+				if(B+C>D) {
+					if(C+D>B) {
+						System.out.println("S");
+						i++;
+					
+					}
+				}
+			}
+			if(A+D>C & i<=0) {
+				if(D+C>A) {
+					if(C+A>D) {
+						System.out.println("S");
+						i++;
+					}
+				}
+			}
+			if(A+B>D & i<=0) {
+				if(B+D>A) {
+					if(D+A>B) {
+						System.out.println("S");
+						i++;
+					}
+				}
+			}
+			if(i==0) {
+				System.out.println("N");
+				
+				
+			}
+
+			
+		
+			
 		
 		
 
--- ArifAhmed/src/Student_main.java
+++ ArifAhmed/src/Student_main.java
@@ -11,3 +11,4 @@ public class Student_main {
 	}
 
 }
+
