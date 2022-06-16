c728200ed3a40d87044d5dddd26791d1687951fc
ArifAhmed <arif15-12301@diu.edu.bd>
2022-06-16 20:19:54 +0600

    SOLVED-1864
    another one done

---
+++ ArifAhmed/src/BEE_1864.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BEE_1864 {
+	static Scanner in = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int N = in.nextInt();
+		String arif = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+		for(int i = 0; i < N; i++ ) {
+			char sec = arif.charAt(i);
+			System.out.print(sec);
+		}
+		System.out.println();
+		
+	}
+
+}
