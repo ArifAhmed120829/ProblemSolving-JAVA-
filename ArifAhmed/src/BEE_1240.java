e2eae97c2b871b1ab318d102a99607b69e533c7d
ArifAhmed <arif15-12301@diu.edu.bd>
2022-07-17 17:09:13 +0600

    1240-bee solved
    solved medium coding

--- ArifAhmed/src/BEE_1240.java
+++ ArifAhmed/src/BEE_1240.java
@@ -6,24 +6,37 @@ public class BEE_1240 {
 		Scanner in = new Scanner(System.in);
 		int N = in.nextInt();
 		for(int  i =0 ; i<N; i++) {
-			int A = in.nextInt();
-			int B = in.nextInt();
+			int A = in.nextInt();//5678690
+			int B = in.nextInt();//78690
 			String res="";
-			String A1 = String.valueOf(A);
-			int length_of_A = A1.length();
-			String B1 = String.valueOf(B);
-			int length_of_B = B1.length();
+			String A1 = String.valueOf(A);//5678690
+			int length_of_A = A1.length();//7
+			String B1 = String.valueOf(B);//78690
+			int length_of_B = B1.length();//5
+			int c = length_of_A - length_of_B;
 			if(A==B) {
 				System.out.println("encaixa");
 			}
-			else if(length_of_A>=length_of_B) {
-				for(int j =length_of_A-1; j>length_of_B;j-- ) {
-					char g = A1.charAt(j);
-					res = res + g;
+			/////
+			else if(length_of_A>=length_of_B) {//7>=5
+				for(int j =0; j<length_of_B;j++ ) { //0<5
+					char g = A1.charAt(c);
+					res = res + g;//
+					c++;
 				}
-				System.out.println(res);
+				if(res.equals(B1)) {
+					System.out.println("encaixa");
+				}
+				else {
+					System.out.println("nao encaixa");
+				}
 				
 			}
+			/////
+			else if(length_of_A<length_of_B) {
+				System.out.println("nao encaixa");
+			}
+			///
 			
 		}
 
