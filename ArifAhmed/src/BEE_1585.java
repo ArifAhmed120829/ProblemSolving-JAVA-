f06d0b4fc4391a7f03056e70c0a0a9554a15cbb4
ArifAhmed <arif15-12301@diu.edu.bd>
2022-07-25 19:19:16 +0600

    Update BEE_1585.java
    added wrapper as well

--- ArifAhmed/src/BEE_1585.java
+++ ArifAhmed/src/BEE_1585.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class BEE_1585 {
 
 	public static void main(String[] args) {
+		
 		Scanner in = new Scanner(System.in);
 		int test_case = in.nextInt();
 		for(int i = 0; i< test_case; i++) {
@@ -12,6 +13,15 @@ public class BEE_1585 {
 			System.out.println(area+" cm2");
 			
 		}
+		/*
+		Wrapper Class
+		********
+		int j = 100;
+		Integer k = Integer.valueOf(j);
+		System.out.println(j+" "+k);
+		*********
+		*/
+		
 
 	}
 
