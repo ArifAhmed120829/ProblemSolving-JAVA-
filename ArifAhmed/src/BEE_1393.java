2d4b65e2fee10493f66c90e9fbad5abd10464c57
ArifAhmed <arif15-12301@diu.edu.bd>
2022-08-27 12:02:53 +0600

    solved_1393
    concept was hard to get but solving was easy

--- ArifAhmed/src/BEE_1393.java
+++ ArifAhmed/src/BEE_1393.java
@@ -5,10 +5,31 @@ public class BEE_1393 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int x;
-		do {
+		
+		 
+		while(true) {
+			int l = 1;
+			int first = 0;
+			int res = first + l;
 			x = in.nextInt();
+			if(x==0) break;
+			else if(x>1) {
+				for(int i = 1; i<x; i++) {
+					first = l;//first = 1,1,2
+					
+					l = res;// l = 1,2,3
+					
+					res = first+l;//res =2,3,5
+				}
+				System.out.println(res);
+			}
+			else if(x==1) {
+				System.out.println(res);
+			}
 			
-		}while(x!=0);
+		}
+			
+		
 
 	}
 
