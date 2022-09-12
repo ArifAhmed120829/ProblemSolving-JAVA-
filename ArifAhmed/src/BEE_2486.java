567f230a8fe1125d6e57822ec2b317cee55dbd1f
ArifAhmed <arif15-12301@diu.edu.bd>
2022-09-13 02:45:19 +0600

    edited

--- ArifAhmed/src/BEE_2486.java
+++ ArifAhmed/src/BEE_2486.java
@@ -28,33 +28,39 @@ public class BEE_2486 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException  {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		String[] space = new String[2];
+		String[] space;
 		int sum;
 		int x;
 		String y;
 		int real_result;
 		while(true) {
-			
 			sum = 0;
 			int result = 0;
 			int t = Integer.parseInt(in.readLine());
 			if(t==0) break;
 			for(int i = 0; i<t; i++) {
+				y = "";
 				space = in.readLine().split(" ");
+				int len = space.length;
+				
 				x = Integer.parseInt(space[0]);
-				y = space[1];
+				for(int p = 1; p<len; p++) {
+					y = y+ space[p]+" ";
+				}
+				y = y.trim();//removing leading and trailing blank spaces meaning first and last spaces
+				
 				switch(y) {
-				case "suco":
+				case "suco de laranja":
 					result = suc_laranja(y)*x;
 					
 					break;
-				case "morango":
+				case "morango fresco":
 					result = morango(y)*x;
 					break;
 				case "mamao":
 					result = mamao(y)*x;
 					break;
-				case "goiaba":
+				case "goiaba vermelha":
 					result = goiaba(y)*x;
 					break;
 				case "manga":
@@ -68,6 +74,7 @@ public class BEE_2486 {
 					break;
 				}
 				sum = sum + result;
+				//System.out.println(result);
 				
 				
 			
