0cb416804023a3cb3e58368b3bca96bd7f6f182d
ArifAhmed <arif15-12301@diu.edu.bd>
2022-09-16 14:13:33 +0600

    bee_2581
    solved

---
+++ ArifAhmed/src/BEE_2581.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BEE_2581 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int test_case = in.nextInt();
+		in.nextLine();
+		for(int i = 0; i<test_case; i++) {
+			String l = in.nextLine();
+			System.out.println("I am Toorg!");
+		}
+
+	}
+
+}
---
+++ ArifAhmed/src/Data_types.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Data_types {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		int x = scan.nextInt();
+		double y= scan.nextDouble();
+		scan.nextLine();
+		String h = scan.nextLine();
+		System.out.println(i+x);
+		System.out.println(d+y);
+		System.out.println(s+h);
+		
+		//solved
+		
+
+	}
+
+}
