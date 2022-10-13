d6281a93514a6918a7484928830fd8dfed77d0a9
ArifAhmed <arif15-12301@diu.edu.bd>
2022-10-13 14:09:21 +0600

    solved
    :)

--- ArifAhmed/src/BEE_2523.java
+++ ArifAhmed/src/BEE_2523.java
@@ -7,15 +7,12 @@ public class BEE_2523 {
 	    while(in.hasNext()) {
 	      String s = in.next();
 	      String result = "";
-	      char a[] = new char[26];
 	      int number = in.nextInt();
 	      for(int j = 0; j<number; j++) {
 	        int x = in.nextInt();
-	        a[j] = s.charAt(x-1);
+	        result  = result + s.charAt(x-1);
 	      }
-	      for(char value: a ) {
-	        result = result + value;
-	      }
+	    
 	      System.out.println(result);
 	    }
 
