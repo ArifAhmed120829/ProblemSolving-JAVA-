2e2bc991615ca141ce94b512870808a89f834a57
ArifAhmed <arif15-12301@diu.edu.bd>
2022-10-18 14:32:32 +0600

    Solved
    ez

---
+++ ArifAhmed/src/BEE_2756.java
@@ -0,0 +1,79 @@
+
+public class BEE_2756 {
+
+	public static void main(String[] args) {
+		for(int  i = 0; i<7; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("A");
+		System.out.println();
+		for(int  i = 0; i<6; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("B");
+		System.out.print(" ");
+		System.out.print("B");
+		System.out.println();
+		for(int  i = 0; i<5; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("C");
+		for(int  i = 0; i<3; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("C");
+		System.out.println();
+		for(int  i = 0; i<4; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("D");
+		for(int  i = 0; i<5; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("D");
+		System.out.println();
+		for(int  i = 0; i<3; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("E");
+		for(int  i = 0; i<7; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("E");
+		System.out.println();
+		for(int  i = 0; i<4; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("D");
+		for(int  i = 0; i<5; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("D");
+		System.out.println();
+		for(int  i = 0; i<5; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("C");
+		for(int  i = 0; i<3; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("C");
+		System.out.println();
+		for(int  i = 0; i<6; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("B");
+		System.out.print(" ");
+		System.out.print("B");
+		System.out.println();
+		for(int  i = 0; i<7; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("A");
+		System.out.println();
+		
+		
+
+	}
+
+}
