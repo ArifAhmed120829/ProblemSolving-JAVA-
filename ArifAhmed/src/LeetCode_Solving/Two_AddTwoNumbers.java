aaf4c0b245632250310e06f2a5a70037d9479021
ArifAhmed <arif15-12301@diu.edu.bd>
2022-11-05 19:24:57 +0600

    trying
    :)

--- ArifAhmed/src/LeetCode_Solving/Two_AddTwoNumbers.java
+++ ArifAhmed/src/LeetCode_Solving/Two_AddTwoNumbers.java
@@ -1,32 +1,59 @@
 package LeetCode_Solving;
- class ListNode {
-	 public int data;
-	 public ListNode objPointer;
-	 //Constructor
-	 public ListNode(int data2, ListNode Pointer) {
-		 //Assigning
-		 this.data = data2;
-		 this.objPointer  = Pointer;
-	 }
-	 //Method
-	 public String tostring() {
-		 return data+"";
-	 }
-	
-}
- class Solutionzzzz {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		return l2;
-        
-    }
-}
+
+
 
 public class Two_AddTwoNumbers {
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+
+		}
+
+		ListNode(int val) {
+			this.val = val;
+
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+		ListNode head = new ListNode(0);
+		ListNode l3 = head;
+		int carry = 0;
+		while (l1 != null || l2 != null) {
+			int l1_val = (l1 != null) ? l1.val : 0;
+			int l2_val = (l2 != null) ? l2.val : 0;
+			int current_sum = l1_val + l2_val + carry;
+			carry = current_sum / 10;
+			int last_digit = current_sum % 10;
+			ListNode newnode = new ListNode(last_digit);
+			l3.next = newnode;
+			if (l1 != null)
+				l1 = l1.next;
+			if (l2 != null)
+				l2 = l2.next;
+			l3 = l3.next;
+		}
+		if (carry > 0) {
+			ListNode newnode = new ListNode(1);
+			l3.next = newnode;
+			l3 = l3.next;
+		}
+		return head.next;
+
+	}
 
 	public static void main(String[] args) {
-		ListNode show = new ListNode (25, null);
-		System.out.println(show);
+		ListNode l1;
 		
+
 	}
 
 }
---
+++ ArifAhmed/src/LeetCode_Solving/p1.java
@@ -0,0 +1,23 @@
+package LeetCode_Solving;
+
+public class p1 {
+	class Node{
+		int data;
+		Node next;
+		
+		
+		Node(int data){
+			this.data = data;
+			this.next  = null;
+		}
+		
+	}
+	public void addFirst(int data) {
+		Node newNode = new Node(data);
+		Node Head = null;
+		if(Head == null) {
+			
+		}
+	}
+
+}
