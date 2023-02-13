dbdb54842729c26d866f73e9cb7f8c610068c684
ArifAhmed <arif15-12301@diu.edu.bd>
2023-02-13 19:14:47 +0600

    updated_code
    solved

--- ArifAhmed/src/LeetCode_Solving/concatenation.java
+++ ArifAhmed/src/LeetCode_Solving/concatenation.java
@@ -1,11 +1,13 @@
 package LeetCode_Solving;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 class Solution8285 {
-    public int findTheArrayConcVal(int[] nums) {
+    public long findTheArrayConcVal(int[] nums) {
     	Stack<Integer> st = new Stack<Integer>();//[2,52,4,7],[2,4,8]
+    	Queue<Integer> qu = new LinkedList<>();
     	long res = 0;
     	int j;
     	int q;
@@ -37,22 +39,29 @@ class Solution8285 {
     		j = nums.length/2-1;
     		q = nums.length/2+1;
     		
-    		
     		for(int i = nums.length/2-1; i>=0; i--) {
-        		st.push(nums[j]);
-        		st.push(nums[q]);
+        		qu.offer(nums[j]);
+        		qu.offer(nums[q]);
         		j--;
         		q++;
         		
         	}
-    		st.push(nums[nums.length/2]);
+    		String[] arr = new String[len/2];
+    		int[] arr2 = new int[len/2];
+    		for(int x = 0; x<len/2; x++) {
+    			arr[x] = String.valueOf(qu.poll())+String.valueOf(qu.poll());
+    		}
+    		for(int x = 0; x<len/2; x++) {
+    			arr2[x] = Integer.parseInt(arr[x]);
+    		}
+    		for(int val:arr2) {
+    			res = res + val;
+    		}
+    		
+    		res = res + nums[nums.length/2];
     		
     	}
-    	System.out.println(st);
-    	return st.size();
-    	
-    	
-        
+    	return res;   
     }
 }
 
@@ -60,7 +69,7 @@ public class concatenation {
 
 	public static void main(String[] args) {
 		Solution8285 ob = new Solution8285();
-		int[] arr = {7,52,2,10,12};
+		int[] arr = {7,52,2,4};//673
 		System.out.println(ob.findTheArrayConcVal(arr));
 	}
 
